79de84ed8a61fa8e5aa659174a65d4aebbacab16
Ryan McLincha <ryanmclincha@gmail.com>
2022-03-09 22:09:40 -0500

    added some comments to the API view and deleted user.py

--- user.py
+++
@@ -1,7 +0,0 @@
-class User:
-    def __init__(self, fname, lname, gender, state, age):
-        self.fname = fname
-        self.lname = lname
-        self.gender = gender
-        self.state = state
-        self.age = age
