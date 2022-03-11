535cdf9c04d55222671bae0e91b3d1afd7b3a455
Ryan McLincha <ryanmclincha@gmail.com>
2022-03-10 23:10:12 -0500

    Added some information to the README.md and changed how the plain text response is handled (No temp buildup)

---
+++ test_processuserdata.py
@@ -0,0 +1,19 @@
+import unittest
+import processuserdata
+from app import process_user_stats
+
+class TestProcessUserData(unittest.TestCase):
+
+    def setUp(self):
+        
+        self.small_json_data_set_1
+        
+
+    def test_get_gender_distribution(self):
+        with open('test_files/', 'f') as f:
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
